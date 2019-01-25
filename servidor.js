9d5a4501f59605401ca8e82f0eb7b0e27a3a977e
DaniGuerra <daniguerra13@gmai.com>
2019-01-25 08:52:49 -0600

    .

---
+++ servidor.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var aplication = express();
+
+const net=require('net')
+const server = require('http').Server(aplication)
+const socket = require('socket.io')(server)
+    const {StringDecoder} = require('string_decoder')
+    const decoder = new StringDecoder('utf8')
+
+var HOST = "192.168.43.150"
+var PORT = 4000
+
+
+server.listen(PORT,function(){
+    console.log('Servidor Activo ' + HOST + ':' + PORT)
+})
+
+var ser = net.createServer(function(so){
+
+    so.on('connect',function(){
+
+        console.log('Nuevo Usuario ' + so.remoteAddress + ':' + so.remotePort)
+        
+    })
+
+    so.on('data',function(data){
+        var cent = data
+
+        so.write("servidor 1");
+    })
+
+    so.on('close',function(){
+        console.log('usuario desconectado')
+    })
+})
+
+ser.listen(PORT,HOST)
